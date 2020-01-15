5abc3b4d27bc62b1eaeb12decc9d37b6ce689cca
qiushen27755 <qiu502639753@163.com>
2020-01-15 16:25:02 +0800

    测试版本第一版,差不多完了

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
     yarn add babel-plugin-import --save-dev
     yarn add react-app-rewired customize-cra --save-dev
     yarn add antd-mobile --save
+
+
+
+    
--- src/application/unclaimed/index.js
+++ src/application/unclaimed/index.js
@@ -10,15 +10,15 @@ class Unclaimed extends Component {
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2
         }); 
-        
         this.state = { 
             dataSource:ds,
             list:[],
+            body:[],
             changeCode:{},
             changeInfo:false, // 换页面
             runListView:true, 
-            pullLoading:false,
-            upLoading:false,
+            pullLoading:false, //上拉
+            upLoading:false, //下拉
             goBack:false,
          }
         this.connBoot=this.connBoot.bind(this)
@@ -81,21 +81,38 @@ class Unclaimed extends Component {
           "state":Number(status)
         },   
        success: res=>{
+         if(Number(index)>0){
           const list=JSON.parse(JSON.stringify(res))
-          const dataSource=this.state.dataSource.cloneWithRows(list.data)
+          let stateList=this.state.body
+           console.log(stateList)
+          const data=stateList.concat(list.data)
+          console.log(data)
+          const dataSource=this.state.dataSource.cloneWithRows(data)
           this.setState({
               dataSource:dataSource,
               list:list,
-              pullLoading:false,
-              upLoading:false,
+              body:data,
+              pullLoading:false // 上拉
           })
+         }else{
+           const list=JSON.parse(JSON.stringify(res))
+           const dataSource=this.state.dataSource.cloneWithRows(list.data)
+           this.setState({
+               dataSource:dataSource,
+               list:list,
+               body:list.data,
+               upLoading:false, //下拉
+           })
+         }
       },
       error:error => Toast.fail('Load failed'+error, 1) })
      }
     render() { 
-        const {runListView,list,dataSource,upLoading,pullLoading,changeInfo} =this.state
+        const {list,dataSource,body,changeInfo} =this.state
+        const {runListView,upLoading,pullLoading}=this.state
+        console.log(body.length)
          const separator=(sectionID,rowID)=>
-        (  
+        (
             <div 
             key={`${sectionID}-${rowID}`}
                 style={{
@@ -110,7 +127,7 @@ class Unclaimed extends Component {
         return ( 
             <div>
             {
-              runListView && list.data? 
+              runListView && body? 
                <ListView 
                   className="am-list"
                   dataSource={dataSource}
@@ -118,49 +135,47 @@ class Unclaimed extends Component {
                   // initialListSize   //这里我没有设置初始值，默认是10个
                   // renderHeader={this.handHead}
                   renderFooter={() => (   
-                                          <div style={{ padding: 30, textAlign: 'center' }}>
-                                          <div>
-                                          {  upLoading ?
-                                          <Icon type="loading" /> : list.pageNum+1===list.pageTotal ?
-                                          <div>-没有更多了-</div> : <div>-{list.pageNum+1}-</div> }
-                                          </div>
-                                          </div>
+                        <div style={{ padding: 30, textAlign: 'center' }}>
+                        <div>
+                        {   
+                        pullLoading ?<Icon type="loading"/> :
+                        list.pageNum+1===list.pageTotal ? <div>-没有更多了-</div> : <div>-{list.pageNum+1}-</div> }
+                        </div>
+                        </div>
                                           )} // render 这两个是针对 listView 的上下 内容的渲染
 
                   renderRow={(rowData, id, i) => this.renderRow(rowData,i)} // 通过参数 方法对 一行进行单独渲染
                   renderSeparator={separator} //渲染了 view之间的小间隙
-
-                  pageSize={list.pageSize}  //每次循环事件 渲染的行数
+                  pageSize={body.length}  //每次循环事件 渲染的行数
                   // renderBodyComponent={() => <MyBody />}
-                   useBodyScroll  //使用html的body 作为滚动容器
+                  useBodyScroll  //使用html的body 作为滚动容器
                   // onScroll={(e) => { console.log('.......'); }} // 列表渲染每行可通过此进行回调
-                  scrollRenderAheadDistance={200} //定制每行接近屏幕范围
                   onEndReached={() => this.onEndReached(list.pageNum, list.pageTotal)} // 自动渲染机制
-                  onEndReachedThreshold={200}
-                  pullToRefresh={<PullToRefresh  
-                                  refreshing={pullLoading}
-                                  onRefresh={this.onRefresh}
-                              />}
+                  onEndReachedThreshold={100}
+                  pullToRefresh={ <PullToRefresh  
+                                              refreshing={upLoading}
+                                              onRefresh={this.onRefresh}
+                                              /> 
+                                }
               />   
               :  changeInfo ? <div> 
                 <UnInfoList param={this.state.changeCode} 
                             runPage={this.runPage}
                             /> 
                 </div> :  <Loading></Loading>
-             }
+            }
            </div>
          ); // 后续最后的三目更换位置
     }
       runPage(){
         this.setState({
           runListView:true,
-          
         })
       }
      //暂定 开始 进行中 结算完毕
    runState=(approve)=>{
      let status=Number(approve)
-     if(status===1){
+     if(status===1 || status===0){
        return <span style={{"marginLeft":"3px","fontSize":"10px",
               "backgroundColor":"#8EE5EE","color":"#595959"}}>{''} 未认领{''} </span>
      }else if(status===2){
@@ -175,21 +190,16 @@ class Unclaimed extends Component {
     //上拉
     onEndReached=(page,lastPage)=>{
     if(Number(page)<Number(lastPage)-1){
-        this.setState({
-            pullLoading:true
-        }) 
+        this.setState({pullLoading:true})
         let index= page+1
         this.connBoot({index:index,status:0})
-    }
+      }
     }
     //下拉
     onRefresh=()=>{
-        this.setState(
-            {upLoading:true}
-        )
-        this.connBoot({index:0,status:0})
+      this.setState({upLoading:true})
+      this.connBoot({index:0,status:0})
       }
 }
  
-// export default withRouter(Unclaimed);
 export default Unclaimed
